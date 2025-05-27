b2c3a3a9a9ca1b71f86ead83e4cc2d06ed01bd79
MariusNWK <marius.nowak@epitech.eu>
2025-05-27 21:39:40 +0200

    fix: timezone + label

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,11 +6,12 @@ export async function fetchTodos(): Promise<Todo[]> {
 	const nowString = now.toLocaleString("fr-FR", {
 		dateStyle: "full",
 		timeStyle: "medium",
+		timeZone: "Europe/Paris",
 	});
 	const uniqueId = faker.string.uuid().slice(0, 8);
 	const fakeTodos: Todo[] = Array.from({ length: 5 }).map((_, i) => ({
 		id: i + 1,
-		title: `${faker.lorem.sentence(3)} (id: ${uniqueId}, généré à: ${nowString})`,
+		title: `${faker.lorem.sentence(3)} (id: ${uniqueId}, généré ${nowString})`,
 		completed: faker.datatype.boolean(),
 		userId: faker.number.int({ min: 1, max: 3 }),
 		date: nowString,
